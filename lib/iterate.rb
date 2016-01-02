2978fb5d5efe547cd9ea709a50793c80820c24ae
Jan Lelis <mail@janlelis.de>
2016-01-02 16:18:22 +0100

    Refactor

--- lib/iterate.rb
+++ lib/iterate.rb
@@ -3,26 +3,15 @@ require_relative "iterate/version"
 module Kernel
   private
 
-  def iterate(*params)
-    # params.shift.zip(*params).each{ |*elements| yield *elements }
-    raise ArgumentError, "wrong number of arguments (0)" if params.empty?
-
-    first = params.shift
-    if params.empty? # single param - like each
-      if block_given?
-        first.map{|e| yield e }
-      else
-        first.map.to_enum
-      end
-    else # multiple params
-      max_size = [first, *params].max_by(&:count).size
-      padded_first = first.to_a + [nil]*(max_size - first.count)  # append nils
-      obj = padded_first.zip(*params)
-      if block_given?
-        obj.map{|es| yield(*es) }
-      else
-        obj.map.to_enum
-      end
+  def iterate(*iteratables)
+    raise ArgumentError, "wrong number of arguments (0)" if iteratables.empty?
+    first, rest = iteratables[0], iteratables[1..-1]
+    if rest.empty?
+      block_given? ? first.map{ |e| yield e } : first.map.to_enum
+    else
+      padding = iteratables.max_by(&:size).size - first.size
+      iter = (first.to_a + [nil]*padding).zip(*rest)
+      block_given? ? iter.map{|es| yield(*es) } : iter.map.to_enum
     end
   end
 end
